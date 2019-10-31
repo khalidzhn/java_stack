a8eaa716b680de56837ea90ddebfedcf3e7f0e01
khalidzhn <43253007+khalidzhn@users.noreply.github.com>
2019-10-31 15:15:28 +0300

    PythagoreanTheorem is done

---
+++ java_fundamentals/PythagoreanTheorem/src/Pythagorean.java
@@ -0,0 +1,10 @@
+import java.lang.Math;
+
+public class Pythagorean {
+
+    public double calculateHypotenuse(int legA, int legB) {
+        double legC=  Math.sqrt(Math.pow(legA,2)+ Math.pow(legB,2) );
+        return legC;
+    }
+
+}
---
+++ java_fundamentals/PythagoreanTheorem/src/PythagoreanTest.java
@@ -0,0 +1,7 @@
+public class PythagoreanTest {
+    public static void main(String[] args) {
+        Pythagorean calculate = new Pythagorean();
+        System.out.println(calculate.calculateHypotenuse(3,4));
+
+    }
+}
