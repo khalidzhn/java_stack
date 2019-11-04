98e6c29b00261968289c95daf5d7fdd942a504ea
khalidzhn <43253007+khalidzhn@users.noreply.github.com>
2019-11-04 21:50:42 +0300

    HomeServlet

---
+++ java_fundamentals/HelloWorld/src/com/example/samples/HomeServlet.java
@@ -0,0 +1,22 @@
+package com.example.samples;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "HomeServlet", urlPatterns = {"/a/b/c"})
+public class HomeServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.write("<h1>Hellllo</h1>");
+    }
+}
